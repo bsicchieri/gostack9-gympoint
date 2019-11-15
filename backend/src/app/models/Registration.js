824102a712917bf4dbe1fe30178cd58fee3b0e12
bsicchieri <brunosicchieri@hotmail.com>
2019-11-15 14:58:48 -0300

    backend

---
+++ backend/src/app/models/Registration.js
@@ -0,0 +1,39 @@
+import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
+
+class Registration extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        start_date: Sequelize.DATE,
+        end_date: Sequelize.DATE,
+        price: Sequelize.NUMBER,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Student, { foreignKey: 'student_id', as: 'student' });
+    this.belongsTo(models.Plan, { foreignKey: 'plan_id', as: 'plan' });
+    this.belongsTo(models.Plan, { foreignKey: 'price', as: 'price' });
+  }
+}
+
+export default Registration;
